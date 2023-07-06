5a29feb2016ed4d629439daeb02c6730f1b1cdbd
setak482 <yumsan1305@gmail.com>
2023-07-06 11:27:47 +0900

    나는야 포켓몬 마스터 이다솜 map -> unordered_map 216ms -> 140ms

--- Silver/S4/1620 나는야 포켓몬 마스터 이다솜.cpp
+++ Silver/S4/1620 나는야 포켓몬 마스터 이다솜.cpp
@@ -1,29 +1,23 @@
-#include <iostream>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
+#define um unordered_map
 using namespace std;
+string reverse_pokemon_dic[100001];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     int input_dic, problem;
     cin >> input_dic >> problem;
-    map<string, int> pokemon_dic;
-    map<int, string> reverse_pokemon_dic;
-    for(int i = 1; i < input_dic+1; i++){
+    um<string, int> pokemon_dic;
+    for(int i = 1; i <= input_dic; i++){
         string a;
         cin >> a;
         pokemon_dic.insert({a, i});
-        reverse_pokemon_dic.insert({i, a});
+        reverse_pokemon_dic[i] = a;
     }
     for(int i = 0; i < problem; i++){
-        string a;
-        cin >> a;
-        if(atoi(a.c_str()) == 0){
+        string a; cin >> a;
+        if(atoi(a.c_str()) == 0)
             cout << pokemon_dic.at(a) << '\n';
-        }
-        else{
-            cout << reverse_pokemon_dic.at(atoi(a.c_str())) << '\n';
-        }
+        else cout << reverse_pokemon_dic[atoi(a.c_str())] << '\n';
     }
 }
